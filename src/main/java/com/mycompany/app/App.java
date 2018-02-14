8418070f966973e38093931def48e092f4c37b8a
samilkahraman <msamilkahraman@gmail.com>
2018-02-15 00:29:32 +0300

    4 parametreli metod eklendi

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -34,6 +34,7 @@ public class App
             String input1 = req.queryParams("input1");
             java.util.Scanner sc1 = new java.util.Scanner(input1);
             sc1.useDelimiter("[;\r\n]+");
+
             java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
             while (sc1.hasNext())
             {
@@ -43,13 +44,37 @@ public class App
             System.out.println(inputList);
 
 
-            String input2 = req.queryParams("input2").replaceAll("\\s","");
-            int input2AsInt = Integer.parseInt(input2);
+            String input2 = req.queryParams("input2");
+            java.util.Scanner sc2 = new java.util.Scanner(input2);
+            sc2.useDelimiter("[;\r\n]+");
 
-            boolean result = App.search(inputList, input2AsInt);
+            java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
+            while (sc2.hasNext())
+            {
+                int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+                inputList2.add(value);
+            }
+            System.out.println(inputList2);
+
+
+            String input3 = req.queryParams("input3").replaceAll("\\s","");
+            System.out.println(input3);
+
+
+
+
+
+
+
+            String input4 = req.queryParams("input4").replaceAll("\\s","");
+
+
+            String sifreliMetin = sifrele(inputList,inputList2,input3,input4);
+
+           System.out.println(sifreliMetin);
 
             Map map = new HashMap();
-            map.put("result", result);
+            map.put("result", sifreliMetin);
             return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
 
@@ -63,6 +88,49 @@ public class App
                 new MustacheTemplateEngine());
     }
 
+    public static String sifrele(ArrayList<Integer> bir, ArrayList<Integer> iki, String mesaj, String sonuc){
+        sonuc="";
+        //birinci listeyi al
+        //ikinci listeyi al
+        // mesajın karakterlerine sırasıyla ilk birincinin birinci değerini topla sonra ikincinin birinci değerini topla
+        //bu şekilde devam ederek çoklu kaydırma ile şifrele sonuca yaz
+        // ornek vermek gerekirse birinci listede 1,2,3 var ikinci listede 4,5,6 var mesajımızda gerede ise sifreli mesaj
+        // g + 1, e + 4, r + 2, e + 5, d + 3, e + 6
+
+        int birinciArrayUzunlugu=bir.size();
+        int ikinciArrayUzunlugu=iki.size();
+        int counter1=0;
+        int counter2=0;
+
+
+        char c;
+        for(int i=0;i<mesaj.length();i++){
+            c=mesaj.charAt(i);
+            if(c%2==0){
+                if(counter1==birinciArrayUzunlugu){
+                    counter1=0;
+                }
+                int deneme=bir.get(counter1);
+                c += deneme;
+
+                sonuc+=c ;
+                counter1++;
+
+            }else{
+                if(counter2==ikinciArrayUzunlugu){
+                    counter2=0;
+                }
+                int deneme=bir.get(counter2);
+                c += deneme;
+
+                sonuc+=c ;
+                counter2++;
+            }
+        }
+
+        return sonuc;
+    }
+
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
@@ -71,4 +139,3 @@ public class App
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
-
